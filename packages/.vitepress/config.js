78ae14c5868cf14503ac6309ab1ed759dd31ca66
leijialiang <ljl519525771@qq.com>
2021-11-24 15:00:38 +0800

    feat: 增加 doc

---
+++ packages/.vitepress/config.js
@@ -0,0 +1,17 @@
+const config = {
+	title: 'rayuse',
+	description: '一些工具函数',
+	themeConfig: {
+		repo: 'SuperRay3/utils',
+		docsDir: 'packages',
+    editLinks: true,
+    editLinkText: '在 Github 上编辑此页面',
+    lastUpdated: '上次更新时间',
+		nav: [
+      { text: '首页', link: '/' },
+      { text: '快速开始', link: '/guide' },
+    ],
+	}
+}
+
+module.exports = config
---
+++ packages/index.md
@@ -0,0 +1,17 @@
+---
+home: true
+actionText: 所有函数
+actionLink: /functions
+
+altActionText: 快速开始
+altActionLink: /guide
+
+features:
+  - title: ⚡ tree shaking
+    details: 只取你想要的
+  - title: 🦾 强壮的类型支持
+    details: 使用 Typescript 编写
+  - title: ☁️ 无需打包
+    details: 可以直接使用 CDN
+footer: MIT Licensed | Copyright © 2021-PRESENT Ray
+---
