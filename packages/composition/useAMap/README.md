6a7d6124550bd20741418ea7982ad34f89b1e743
leijialiang <ljl519525771@qq.com>
2021-12-09 15:30:46 +0800

    feat: 增加 useAMap doc

--- packages/composition/useAMap/README.md
+++ packages/composition/useAMap/README.md
@@ -1,6 +1,34 @@
-
 # useAMap
 
-方便、快捷的使用高德地图 api
+确保高德 JS API 加载完成后再进行自定义操作，通过提供 `onMapLoaded` 允许注册自定义操作。
 
+```vue
+<script setup lang="ts">
+  import { ref, unref } from "vue"
+  import { useAMap } from "@rayuse/composition";
 
+  const mapRef = ref<null | HTMLDivElement>(null)
+  const { onMapLoaded, map, loader } = useAMap(mapRef, {
+    key: 'xxxxxxxxxxxxx',
+    version: '2.0',
+    plugins: ['AMap.Scale', 'AMap.ToolBar']
+  })
+
+  onMapLoaded(() => {
+    map.value.addControl(new loader.value.Scale())
+    map.value.addControl(new loader.value.ToolBar())
+  })
+  
+</script>
+
+<template>
+  <div ref="mapRef" class="map-wrapper"></div>
+</template>
+
+<style scoped>
+  .map-wrapper {
+    height: 250px;
+  }
+</style>
+
+```
--- packages/composition/useAMap/index.ts
+++ packages/composition/useAMap/index.ts
@@ -1,62 +1,76 @@
-
 import { ref, Ref, watch } from 'vue-demi'
 
-import AMapLoader from '@amap/amap-jsapi-loader'
+import AMapLoader, { load } from '@amap/amap-jsapi-loader'
 
-interface AMapLoaderOptType {
-	key: string; // 申请好的Web端开发者Key，首次调用 load 时必填
-	version: string; // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
-	plugins?: string[]; //插件列表
-	// 是否加载 AMapUI，缺省不加载
-	AMapUI?: {
-			version?: string; // AMapUI 缺省 1.1
-			plugins?: string[]; // 需要加载的 AMapUI ui插件
-	};
-	// 是否加载 Loca， 缺省不加载
-	Loca?: {
-			version?: string; // Loca 版本，缺省 1.3.2
-	};
+export type AMapLoaderOptType = Parameters<typeof load>[0]
+
+/**
+ * 快捷使用 amap-jsapi-loader
+ * @param dom 地图挂在的 dom 节点
+ * @param mapOpt 高德地图配置项
+ * @returns
+ */
+
+export type useAMapType = (dom: Ref<null | HTMLDivElement>, mapOpt: AMapLoaderOptType) => {
+	map: Ref<any>,
+	loader: Ref<any>,
+	loaderLoaded: Ref<boolean>,
+	onMapLoaded: (cb: () => void) => void
 }
-
-interface allMapOpts extends AMapLoaderOptType {
-	// loader 加载完毕后如果 dom 也准备完毕时立即尝试实例化地图
-	immediate?: boolean
-}
-
-export function useAMap(dom: Ref<null | HTMLDivElement>, opt: allMapOpts = {
+export const useAMap: useAMapType = (dom, mapOpt = {
 	key: '',
-	version: '2.0',
-	immediate: true
-}) {
-	if (!opt.key) {
-		console.error('请提供 Web 端开发者Key')
-		return
-	}
-
-
+	version: '2.0'
+}) => {
 	const loaderLoaded = ref<boolean>(false)
-	const loader = ref(null)
-	const map = ref(null)
+	const loader = ref<any>(null)
+	const map = ref<any>(null)
 
-	const mapOnLoaded: Array<() => void> = []
+	let mapOnLoaded: Array<() => void> = []
 	const onMapLoaded = (cb: () => void) => {
 		mapOnLoaded.push(cb)
 	}
 
-	AMapLoader.load(opt).then(AMap => {
-		loaderLoaded.value = true
-		loader.value = AMap
-
-		if (opt.immediate && dom?.value) {
-			map.value = new AMap.Map(dom.value)
-			mapOnLoaded.forEach(cb => cb())
+	watch([loaderLoaded, dom], ([newLoaderLoaded, newDom]) => {
+		// 当 loader 和 dom 都加载完成时再挂载地图
+		if (newLoaderLoaded && newDom) {
+			loadMap()
+				.then(() => {
+					mapOnLoaded.forEach(cb => cb()) // 依次调用注册的地图初始化成功事件
+					mapOnLoaded = []
+				})
+				.catch((error) => {
+					console.error(error)
+				})
 		}
-	}).catch(() => {
-		console.error('amap-jsapi-loader 加载失败！')
 	})
 
-	watch(dom, (newVal) => {
-		console.log(newVal)
+	/**
+	 * 挂载地图
+	 */
+	function loadMap() {
+		return new Promise((res, rej) => {
+			if (!dom.value) {
+				return rej('地图要挂载的 dom 不可用')
+			}
+
+			if (!loaderLoaded.value) {
+				return rej('amap-jsapi-loader 还未加载完成')
+			}
+
+			try {
+				map.value = new loader.value.Map(dom.value)
+				res(map.value)
+			} catch {
+				rej('加载地图失败！')
+			}
+		})
+	}
+
+	AMapLoader.load(mapOpt).then(AMap => {
+		loaderLoaded.value = true
+		loader.value = AMap
+	}).catch(() => {
+		console.error('amap-jsapi-loader 加载失败！')
 	})
 
 	return {
@@ -66,3 +80,5 @@ export function useAMap(dom: Ref<null | HTMLDivElement>, opt: allMapOpts = {
 		onMapLoaded
 	}
 }
+
+export type useAMapReturn = ReturnType<typeof useAMap>
